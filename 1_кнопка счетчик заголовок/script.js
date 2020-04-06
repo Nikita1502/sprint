f86d70336c67afe53e63766b4d31a7661b7719a7
Nikita <Nikitos1502@yandex.ru>
2020-04-06 20:21:23 +0700

    Кнопка имеет задержку 2 секунды

--- 1_кнопка счетчик заголовок/script.js
+++ 1_кнопка счетчик заголовок/script.js
@@ -1,7 +1,9 @@
 let num = 0;
 
-counter.onclick = function() {
+// любое событие, в частности onclick должно быть функцией
+document.getElementById('counter').onclick = function() {
+    setTimeout(function(){
     num++;
     document.querySelector("h1").innerHTML = num;
+}, 2000)
 }
-
