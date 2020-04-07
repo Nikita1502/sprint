549dd4930a5b5fd59b24fe960af42f0efc29c78b
Nikita <Nikitos1502@yandex.ru>
2020-04-07 22:58:33 +0700

    С доп. заданиями

--- 3_массивы/script.js
+++ 3_массивы/script.js
@@ -8,14 +8,57 @@ var scores = [60, 50, 60, 58, 54, 54,
     hiArr = [];
 
 scores.forEach(function(item, i){
-    console.log("Bubble solution #" + i + " score: " + scores[i]);
-    if (scores[i] > highest) highest = scores[i];
+    console.log("Bubble solution #" + i + " score: " + item);
+    if (item > highest) highest = item;
 });
 
 scores.forEach(function(item, i){    
-    if (scores[i] == highest) hiArr.push('#' + i);
+    if (item == highest) hiArr.push('#' + i);
 });
 
 console.log('Bubbles tests: ' + scores.length);
 console.log('Highest bubble score: ' + highest);
 console.log('Solutions with highest score: ' + hiArr);
+
+// Элементы, которые не больше 60
+var under60 = scores.filter(function(number) {
+    return number <= 60;
+  });
+console.log(under60);
+
+// Массив, в котором все элементы отрицательные
+var negative = scores.map(function(number) {
+    return number * -1;
+  });
+console.log(negative);
+
+// Сортированный по убыванию массив, хз как это работает
+ var sorted = scores;
+ function compareNumeric(a, b) {
+    if (a > b) return -1;
+    if (a == b) return 0;
+    if (a < b) return 1;
+  };
+  sorted.sort(compareNumeric);
+  console.log(sorted);
+
+
+// var nums = scores.map(function(s) {
+//     return s * 1.5;
+//   });
+// console.log(nums);
+
+//   var over50 = scores.filter(function(number) {
+//     return number > 50;
+//   });
+// console.log(over50);
+
+//  var isOver10 = scores.every(function(number){
+//      return number > 10;
+//  });
+//  console.log(isOver10);
+
+//  var isOver50 = scores.some(function(number){
+//     return number > 500;
+// });
+// console.log(isOver50);
