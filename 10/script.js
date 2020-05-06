9041e4fb09124870a2ee790fb7ed70e3bd0f2ffb
Nikita <Nikitos1502@yandex.ru>
2020-05-06 15:41:12 +0700

    беды с версткой

--- 10/script.js
+++ 10/script.js
@@ -1,17 +1,30 @@
 var notes = [];
-function noteConstructor(title, date, description){
+var filtredNotes = [];
+
+function Note(title, date, description){
+    this.id = this.count;
     this.title = title;
     this.date = date;
     this.description = description;
+    this.__proto__.count ++;
 }
+Note.prototype.count = 0;
+
+Note.prototype.remove = function(){
+    var noteId = this.id;
+
+    notes = notes.filter(function(item){
+        return item.id !== noteId;
+    });
+}
+
 function addNote(){
-    var title = document.getElementById('title');
-    var date = document.getElementById('date');
-    var description = document.getElementById('description');
-    var note = new noteConstructor(title.value, date.value, description.value);
-    notes.push(note);
-}
-function drawNotes(){
+    var titleInput = document.getElementById('title');
+    var dateInput = document.getElementById('date');
+    var descriptionInput = document.getElementById('description');
+    var newNote = new Note(titleInput.value, dateInput.value, descriptionInput.value);
+    notes.push(newNote);
+
     var main             = document.querySelector('main');
     var note             = document.createElement('div');
     var title            = document.createElement('div');
@@ -31,25 +44,68 @@ function drawNotes(){
     note.append(description);
     note.append(deleteNoteButton);
 
-    deleteNoteButton.innerHTML = "X";
-    Object.keys(note).forEach(function(item, i){
-        title[i].innerHTML = notes[i].title;
-        date[i].innerHTML = notes[i].date;
-        description[i].innerHTML = notes[i].description;
-    });
+    // deleteNoteButton.innerHTML = "X";
+
+    title.innerHTML = newNote.title;
+    date.innerHTML = newNote.date;
+    description.innerHTML = newNote.description;
 
     deleteNoteButton.onclick = function(){
         note.remove();
-        // notes.splice(1, 1);
-        console.log('1');
-        // notes = notes.filter(function(item, index){
-        //     return index !== personIndex;
-        // })
+        newNote.remove();
     }
-
 }
+
+var filter = function(){
+    var search = document.getElementById('search');
+    filtredNotes = notes.filter(function(item){
+        var ifTitle = item.title.toUpperCase().includes(search.value.toUpperCase());
+        var ifDescription = item.description.toUpperCase().includes(search.value.toUpperCase());
+
+        return ifTitle || ifDescription;
+    });
+}
+var printFiltred = function(){
+    document.querySelector('main').innerHTML = '';
+    filtredNotes.forEach(function(item){
+        var main             = document.querySelector('main');
+        var note             = document.createElement('div');
+        var title            = document.createElement('div');
+        var date             = document.createElement('div');
+        var description      = document.createElement('div');
+        var deleteNoteButton = document.createElement('button');
+
+        note.classList.add('note'); 
+        title.classList.add('title'); 
+        date.classList.add('date'); 
+        description.classList.add('description'); 
+        deleteNoteButton.classList.add('deleteNoteButton');
+
+        main.append(note);
+        note.append(title);
+        note.append(date);
+        note.append(description);
+        note.append(deleteNoteButton);
+
+        // deleteNoteButton.innerHTML = "X";  
+        
+        title.innerHTML = item.title;
+        date.innerHTML = item.date;
+        description.innerHTML = item.description;
+
+        deleteNoteButton.onclick = function(){
+        note.remove();
+        item.remove();
+        }
+    });
+}
+
+document.getElementById('search').onkeyup = function(){
+    filter();
+    printFiltred();
+} 
+
 enterBtn.onclick = function(){
     addNote();
-    drawNotes();
-
 }
+
